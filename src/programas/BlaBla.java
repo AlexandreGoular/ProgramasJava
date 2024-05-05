4597b868febc2cc59b8a92749ef29da4c2ff935e
Alexandre_Goulart <131091829+AlexandreGoular@users.noreply.github.com>
2024-05-04 21:03:37 -0300

    Update BlaBla.java

--- src/programas/BlaBla.java
+++ src/programas/BlaBla.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.IOException;
+import java.io.FileWriter;
 
 public class BlaBla {
 
@@ -11,22 +12,28 @@ public class BlaBla {
 
             File arquivo = new File(caminho);
 
+            FileWriter writer = new FileWriter(arquivo);
+
+            writer.write("Faz o LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL");
+            writer.close();
+
             if (!arquivo.exists()) {
                 arquivo.createNewFile();
                 System.out.println("Arquivo criado: " + caminho);
+
             } else {
 
                 int i = 0;
 
                 while (true) {
 
-                    Process comandoEdge = Runtime.getRuntime().exec("cmd /c start msedge");
+                    // Process comandoEdge = Runtime.getRuntime().exec("cmd /c start msedge");
                     Process comandoNotepad = Runtime.getRuntime().exec("notepad " + caminho);
 
 
                     i++;
 
-                    if (i == 1000) {
+                    if (i == 100000) {
                         break;
                     }
 
